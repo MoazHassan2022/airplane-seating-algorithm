59ef536bbd9863b713ef9cfc063e2d5e56d4f114
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-03-19 03:29:42 +0200

    added buildSeatsMatrix & printSeatsLayout to utils

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-/**
- * function initializes seats layout matrix
- * @param airplaneLayout 2D array like [[1,2],[3,6],[2,3],[5,4]]
- * @returns object object contains seatsMatrix which is a 3D array represents the actual airplane seats, and maxRowsNum which is max number of rows
- */
-const buildSeatsMatrix = (airplaneLayout) => {
-  let seatsMatrix = [];
-  let maxRowsNum = 0;
-  airplaneLayout.forEach((section) => {
-    let sectionMatrix = [];
-    for (let i = 0; i < section[1]; i++) {
-      // row by row
-      let sectionRow = [];
-      maxRowsNum = section[1] > maxRowsNum ? section[1] : maxRowsNum;
-      for (let j = 0; j < section[0]; j++) {
-        // column by column
-        sectionRow.push(0);
-      }
-      sectionMatrix.push(sectionRow);
-    }
-    seatsMatrix.push(sectionMatrix);
-  });
-  return { seatsMatrix, maxRowsNum };
-};
+const utils = require("./utils");
 
 /**
  * function distributes the passengers among the seats
@@ -119,22 +96,6 @@ const distributeSeats = (seatsMatrix, maxRowsNum, passengers) => {
 };
 
 /**
- * function prints airplane seats matrix
- * @param seatsMatrix 3D array represents the actual airplane seats
- */
-const printSeatsLayout = (seatsMatrix) => {
-  seatsMatrix.forEach((section) => {
-    console.log("============================================================");
-    section.forEach((row) => {
-      console.log(row);
-      console.log(
-        "------------------------------------------------------------"
-      );
-    });
-  });
-};
-
-/**
  * function handles all the logic for the seating algorithm
  */
 const startSeatingAlgorithm = () => {
@@ -152,9 +113,9 @@ const startSeatingAlgorithm = () => {
       console.error(`Passengers count is not a number!`);
       process.exit(1);
     }
-    let { seatsMatrix, maxRowsNum } = buildSeatsMatrix(airplaneLayout);
+    let { seatsMatrix, maxRowsNum } = utils.buildSeatsMatrix(airplaneLayout);
     seatsMatrix = distributeSeats(seatsMatrix, maxRowsNum, passengers);
-    printSeatsLayout(seatsMatrix);
+    utils.printSeatsLayout(seatsMatrix);
   } catch (error) {
     if (error instanceof SyntaxError) {
       console.error(
