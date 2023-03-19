9732d0c1c51082847405c2d87e8460cda364389c
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-03-19 02:51:14 +0200

    finished distribute seats function

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /**
  * function initializes seats layout matrix
  * @param airplaneLayout 2D array like [[1,2],[3,6],[2,3],[5,4]]
- * @returns object object contains seatsMatrix which is a 3D array represents the actual airplane seats, and maxRowSize which is max number of rows
+ * @returns object object contains seatsMatrix which is a 3D array represents the actual airplane seats, and maxRowsNum which is max number of rows
  */
 const buildSeatsMatrix = (airplaneLayout) => {
   let seatsMatrix = [];
-  let maxRowSize = 0;
+  let maxRowsNum = 0;
   airplaneLayout.forEach((section) => {
     let sectionMatrix = [];
     for (let i = 0; i < section[1]; i++) {
       // row by row
       let sectionRow = [];
-      maxRowSize = section[1] > maxRowSize ? section[1] : maxRowSize;
+      maxRowsNum = section[1] > maxRowsNum ? section[1] : maxRowsNum;
       for (let j = 0; j < section[0]; j++) {
         // column by column
         sectionRow.push(0);
@@ -20,20 +20,101 @@ const buildSeatsMatrix = (airplaneLayout) => {
     }
     seatsMatrix.push(sectionMatrix);
   });
-  return { seatsMatrix, maxRowSize };
+  return { seatsMatrix, maxRowsNum };
 };
 
 /**
  * function distributes the passengers among the seats
  * @param seatsMatrix 3D array represents the actual airplane seats
- * @param maxRowSize a number represents max number of rows in the layout
+ * @param maxRowsNum a number represents max number of rows in the layout
  * @param passengers a number represents passengers count
  * @returns seatsMatrix 3D array represents the actual airplane seats after distribution
  */
-const distributeSeats = (seatsMatrix, maxRowSize, passengers) => {
+const distributeSeats = (seatsMatrix, maxRowsNum, passengers) => {
+  let currentPassengerId = 0;
   // first run, complete the aisle seats
+  for (let rowIndex = 0; rowIndex < maxRowsNum; rowIndex++) {
+    // iterate over the layout row by row
+    seatsMatrix.forEach((section, sectionIndex) => {
+      if (currentPassengerId >= passengers)
+        // all passengers are distributed
+        return seatsMatrix;
+      if (rowIndex < section.length) {
+        // this section is not finished before this row
+        const colsNum = section[rowIndex].length;
+        if (sectionIndex === 0) {
+          // most left section
+          if (colsNum - 1 > 0) {
+            // most right column (aisle)
+            seatsMatrix[sectionIndex][rowIndex][colsNum - 1] =
+              ++currentPassengerId;
+          }
+        } else if (sectionIndex === seatsMatrix.length - 1) {
+          // most right section
+          if (colsNum > 1) {
+            // most left column (aisle)
+            seatsMatrix[sectionIndex][rowIndex][0] = ++currentPassengerId;
+          }
+        } else {
+          // a center section.
+          if (colsNum > 0) {
+            // (aisle)
+            seatsMatrix[sectionIndex][rowIndex][0] = ++currentPassengerId;
+          }
+          if (colsNum - 1 > 0) {
+            // (aisle)
+            seatsMatrix[sectionIndex][rowIndex][colsNum - 1] =
+              ++currentPassengerId;
+          }
+        }
+      }
+    });
+  }
   // second run, complete the window seats
+  for (let rowIndex = 0; rowIndex < maxRowsNum; rowIndex++) {
+    // iterate over the layout row by row
+    seatsMatrix.forEach((section, sectionIndex) => {
+      if (currentPassengerId >= passengers)
+        // all passengers are distributed
+        return seatsMatrix;
+      if (rowIndex < section.length) {
+        // this section is not finished before this row
+        const colsNum = section[rowIndex].length;
+        if (sectionIndex === 0) {
+          // most left section
+          if (colsNum > 0) {
+            // most right column (window)
+            seatsMatrix[sectionIndex][rowIndex][0] = ++currentPassengerId;
+          }
+        } else if (sectionIndex === seatsMatrix.length - 1) {
+          // most right section
+          if (colsNum > 0) {
+            // most left column (aisle)
+            seatsMatrix[sectionIndex][rowIndex][colsNum - 1] =
+              ++currentPassengerId;
+          }
+        }
+      }
+    });
+  }
   // third run, complete the center seats
+  for (let rowIndex = 0; rowIndex < maxRowsNum; rowIndex++) {
+    // iterate over the layout row by row
+    seatsMatrix.forEach((section, sectionIndex) => {
+      if (rowIndex < section.length) {
+        // this section is not finished before this row
+        const colsNum = section[rowIndex].length;
+        for (let col = 1; col < colsNum - 1; col++) {
+          // iterate through this part of the section
+          if (currentPassengerId >= passengers)
+            // all passengers are distributed
+            return seatsMatrix;
+          // center seats
+          seatsMatrix[sectionIndex][rowIndex][col] = ++currentPassengerId;
+        }
+      }
+    });
+  }
 };
 
 /**
@@ -53,8 +134,8 @@ const startSeatingAlgorithm = () => {
     if (typeof passengers !== "number" || isNaN(passengers)) {
       throw new TypeError();
     }
-    let { seatsMatrix, maxRowSize } = buildSeatsMatrix(airplaneLayout);
-    seatsMatrix = distributeSeats(seatsMatrix, maxRowSize, passengers);
+    let { seatsMatrix, maxRowsNum } = buildSeatsMatrix(airplaneLayout);
+    seatsMatrix = distributeSeats(seatsMatrix, maxRowsNum, passengers);
     //printSeatsLayout(seatsMatrix);
   } catch (error) {
     if (error instanceof SyntaxError) {
